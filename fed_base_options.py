002956ae1fcac80623d81897dcb993b1af322b34
BobVo <bobvo22@outlook.com>
2020-05-15 21:13:44 -0400

    force fedavg to run sgd and the cefed run adam

--- fed_base_options.py
+++ fed_base_options.py
@@ -1,21 +1,14 @@
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Python version: 3.6
-
 import argparse
-
-
 def args_parser():
     parser = argparse.ArgumentParser()
-
     # federated arguments (Notation for the arguments followed from paper)
     parser.add_argument('--R', type=int, default=200,
                         help="number of rounds of training")
-    parser.add_argument('--target-acc', type=float, default=70.00,
-                        help='learning rate')
+    parser.add_argument('--target-acc', type=float, default=97.00,
+                        help='target accuracy/convergene target')
     parser.add_argument('--W', type=int, default=5,
-                        help="number of users: ")
+                        help="number of users/worker: ")
     parser.add_argument('--C', type=float, default=0.5,
                         help='the fraction of clients: C')
     parser.add_argument('--E', type=int, default=1,
@@ -30,7 +23,6 @@ def args_parser():
                         help='sparcify rate(default: 0.6)')
     # model arguments
     parser.add_argument('--model', type=str, default='mlp', help='model name')
-    
     # other arguments
     parser.add_argument('--dataset', type=str, default='mnist', help="name \
                         of dataset")
@@ -38,16 +30,11 @@ def args_parser():
                                 of method")
     parser.add_argument('--num_classes', type=int, default=10, help="number \
                         of classes")
-    parser.add_argument('--gpu', default=None, help="To use cuda, set \
-                        to a specific GPU ID. Default set to use CPU.")
     parser.add_argument('--optimizer', type=str, default='adam', help="type \
                         of optimizer")
     parser.add_argument('--iid', type=int, default=1,
                         help='Default set to IID. Set to 0 for non-IID.')
-   
     parser.add_argument('--verbose', type=int, default=1, help='verbose')
     parser.add_argument('--seed', nargs='+', default=[47], help='random seed')
-
-
     args = parser.parse_args()
     return args
